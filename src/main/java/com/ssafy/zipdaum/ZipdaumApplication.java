5ca90f4051e58dd63ebb7a04876a7031a8e38e9e
kyungrin Kim <0123jade0123@gmail.com>
2026-06-08 16:07:20 +0900

    chore: 프로젝트 초기 환경 세팅

---
+++ src/main/java/com/ssafy/zipdaum/ZipdaumApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.zipdaum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipdaumApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ZipdaumApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ssafy/zipdaum/ZipdaumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.zipdaum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipdaumApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
