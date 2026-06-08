dcc64865e9213e546dca0ce4a4c75f643768ee38
kyungrin Kim <0123jade0123@gmail.com>
2026-06-08 20:37:07 +0900

    chore: 전역 예외 처리 구조 추가
    - 예외 코드 정의(ErrorCode.java) 및 공통 응답 DTO(ErrorResponse.java) 구현
    - 비즈니스 예외 처리를 위한 커스텀 예외(BusinessException.java) 구현
    - GlobalExceptionHandler를 통한 전역 예외 제어 및 로깅

---
+++ src/main/java/com/ssafy/zipdaum/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.ssafy.zipdaum.global.exception;
+
+import com.ssafy.zipdaum.global.error.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(BusinessException.class)
+  protected ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e){
+    ErrorCode errorCode = e.getErrorCode();
+
+    log.warn("BusinessException 발생 | 코드: {}, 메시지: {}", errorCode.name(), errorCode.getMessage());
+
+    ErrorResponse response = new ErrorResponse(errorCode);
+    return new ResponseEntity<>(response, errorCode.getStatus());
+  }
+
+  @ExceptionHandler(Exception.class)
+  protected ResponseEntity<ErrorResponse> handleException(Exception e){
+    log.error("예상치 못한 시스템 에러 발생: ", e);
+
+    ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+    ErrorResponse response = new ErrorResponse(errorCode);
+    return new ResponseEntity<>(response, errorCode.getStatus());
+  }
+
+}
