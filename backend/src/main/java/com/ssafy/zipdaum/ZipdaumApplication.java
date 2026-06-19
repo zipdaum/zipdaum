3d934f0e771eb3c8af12ec095ffb6e9217f82a55
cseongbo <cseongbo17@gmail.com>
2026-06-19 13:29:38 +0900

    feat: property데이터 스프링 배치 및 스케쥴러 기능 추가

--- backend/src/main/java/com/ssafy/zipdaum/ZipdaumApplication.java
+++ backend/src/main/java/com/ssafy/zipdaum/ZipdaumApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.zipdaum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ZipdaumApplication {
 
   public static void main(String[] args) {
