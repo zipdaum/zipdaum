9dc216180a8a56bdc22e6779abcafc1081d215ba
강현 <khyeon.dev@gmail.com>
2026-06-22 14:21:07 +0900

    [fix] 중복 어노테이션 제거 (#46)

--- backend/src/main/java/com/ssafy/zipdaum/ZipdaumApplication.java
+++ backend/src/main/java/com/ssafy/zipdaum/ZipdaumApplication.java
@@ -6,7 +6,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-@EnableScheduling
 public class ZipdaumApplication {
 
   public static void main(String[] args) {
