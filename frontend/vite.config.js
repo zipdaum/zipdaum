20c52d45607069f391ac56f4912b62902e888079
cseongbo <105208568+cseongbo@users.noreply.github.com>
2026-06-22 14:14:51 +0900

    [feature] property 공공데이터 스프링 배치 및 스케쥴러 적용 (#36)
    Co-authored-by: 강현 <khyeon.dev@gmail.com>

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,6 +17,10 @@ export default defineConfig({
       '/users': {
         target: 'http://localhost:8080',
         changeOrigin: true
+      },
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
       }
     }
   }
